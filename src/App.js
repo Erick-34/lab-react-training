7c25f3b4872b4e9f9f40c2352c35cbbad7c60b5d
Erick-34 <erickoliveira2060@gmail>
2020-07-21 15:14:56 -0300

    done IdCard

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Card from './Components/Card/Card';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="root">
+      <Card
+        picture="https://randomuser.me/api/portraits/men/44.jpg"
+        firstName="John"
+        lastName="Doe"
+        gender="male"
+        height={178}
+        birth={new Date('1992-07-14')}
+      />
+
+      <Card
+        picture="https://randomuser.me/api/portraits/women/44.jpg"
+        firstName="Obrien"
+        lastName="Delores "
+        gender="female"
+        height={172}
+        birth={new Date('1988-05-11')}
+      />
+      
     </div>
   );
 }
---
+++ src/Components/Card/Card.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './card.css';
+
+function Card(props) {
+  return (
+    <div className="card-container">
+      <img src={props.picture} />
+      <div classname="fisrtname">
+        <label><strong>FirstName: </strong></label>
+        <span>{props.firstName}</span>
+      </div>
+      <div classname="lastname">
+        <label><strong>LastName: </strong></label>
+        <span>{props.lastName}</span>
+      </div>
+      <div classname="gender">
+        <label><strong>Gender: </strong></label>
+        <span>{props.gender}</span>
+      </div>
+      <div classname="height">
+        <label><strong>Height :</strong></label>
+        <span>{props.height}</span>
+      </div>
+      <div classname="birth">
+        <label><strong>Birth :</strong></label>
+        <span>{props.birth.toDateString()}</span>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
